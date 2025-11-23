599e7a94ec85d4e35940e0be8800e6d8ef9361c7
Daniel Ben Porat <daniel.benporat@post.runi.ac.il>
2025-11-23 19:38:46 +0200

    adding primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,35 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
+
+        int min = 2;
+        int max = Integer.parseInt(args[0]);
+        boolean[] primes = new boolean[max + 1];
+        primes[0] = false;
+        primes [1] = false;
+        int count = min;
+        int prime = 2;
+        while (prime > Math.sqrt(max)) {
+            for (int i = prime + 1; i < primes.length;i++) {
+                if (i % prime == 0) {
+                    primes[i] = false; 
+                }
+            }
+            prime = nextPrime(prime++ ,primes);
+        }
+        int numberOfPrimes = primes.length;
+        int precent = (numberOfPrimes / max ) * 100 ;
+        System.out.println("There are " + numberOfPrimes + " primes between " + min + " and " + max +
+                             " (" + precent + "% are primes)");
     }
+}
+public static int nextPrime(int prime, boolean[] primes) {
+    while (true) {
+        if (primes[prime] == true) {
+            return prime;
+        } else {
+            prime++;
+        }
+    }
+    return 1;
 }
