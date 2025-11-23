9d90a68c894d6fc1d5d9e77bc8c0f64f007dce89
Daniel Ben Porat <daniel.benporat@post.runi.ac.il>
2025-11-23 20:07:44 +0200

    addind precent to prime

--- Primes.java
+++ Primes.java
@@ -5,10 +5,11 @@ public class Primes {
         int min = 2;
         int max = Integer.parseInt(args[0]);
         boolean[] primes = new boolean[max + 1];
-        primes[0] = false;
-        primes [1] = false;
         int prime = 2;
-        while (prime > Math.sqrt(max)) {
+        for (int i = 2; i < primes.length;i++) {
+            primes[i] = true;
+        }
+        while (prime < Math.sqrt(max)) {
             for (int i = prime + 1; i < primes.length;i++) {
                 if (i % prime == 0) {
                     primes[i] = false; 
@@ -24,14 +25,14 @@ public class Primes {
                 }
             }
         }
-        int numberOfPrimes = 0;
+        double numberOfPrimes = 0.0;
         for (int i = 0; i < primes.length; i++) {
             if (primes[i] == true) {
                 numberOfPrimes++;
             }
         }
-        int precent = (numberOfPrimes / max ) * 100 ;
-        System.out.println("There are " + numberOfPrimes + " primes between " + min + " and " + max +
+        int precent = (int) (numberOfPrimes / max  * 100) ;
+        System.out.println("There are " + (int)numberOfPrimes + " primes between " + min + " and " + max +
                              " (" + precent + "% are primes)");
         
     } 
